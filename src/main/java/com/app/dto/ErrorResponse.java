b1fe51c4daa3285002a935827bd22ee1ae134de0
Yash <ypatodkar@gmail.com>
2022-04-01 10:11:57 +0530

    Added getters, setters and constructor in the Screen and theatre class

--- src/main/java/com/app/dto/ErrorResponse.java
+++ src/main/java/com/app/dto/ErrorResponse.java
@@ -16,4 +16,50 @@ import lombok.ToString;
 public class ErrorResponse {
 	private String message;
 	private LocalDateTime timeStamp;
+	
+	
+	
+	public ErrorResponse() {
+		super();
+	}
+
+
+
+	public ErrorResponse(String message, LocalDateTime timeStamp) {
+		super();
+		this.message = message;
+		this.timeStamp = timeStamp;
+	}
+
+
+
+	public String getMessage() {
+		return message;
+	}
+
+
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+
+
+	public LocalDateTime getTimeStamp() {
+		return timeStamp;
+	}
+
+
+
+	public void setTimeStamp(LocalDateTime timeStamp) {
+		this.timeStamp = timeStamp;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "ErrorResponse [message=" + message + ", timeStamp=" + timeStamp + "]";
+	}
+	
 }
--- src/main/java/com/app/pojos/Screen.java
+++ src/main/java/com/app/pojos/Screen.java
@@ -34,6 +34,84 @@ public class Screen extends BaseEntity{
 	@OneToMany(mappedBy = "screenId", cascade = CascadeType.ALL, orphanRemoval = true)
 	private List<Seat> seats = new ArrayList<>();
 	
+	
+	
+	
+	public Screen() {
+		super();
+	}
+
+
+
+
+	public Screen(@NotNull(message = "screen number cannot be null") int screenNumber, Theatre theatreId, Show showId,
+			List<Seat> seats) {
+		super();
+		this.screenNumber = screenNumber;
+		this.theatreId = theatreId;
+		this.showId = showId;
+		this.seats = seats;
+	}
+
+
+
+
+	public int getScreenNumber() {
+		return screenNumber;
+	}
+
+
+
+
+	public void setScreenNumber(int screenNumber) {
+		this.screenNumber = screenNumber;
+	}
+
+
+
+
+	public Theatre getTheatreId() {
+		return theatreId;
+	}
+
+
+
+
+	public void setTheatreId(Theatre theatreId) {
+		this.theatreId = theatreId;
+	}
+
+
+
+
+	public Show getShowId() {
+		return showId;
+	}
+
+
+
+
+	public void setShowId(Show showId) {
+		this.showId = showId;
+	}
+
+
+
+
+	public List<Seat> getSeats() {
+		return seats;
+	}
+
+
+
+
+	public void setSeats(List<Seat> seats) {
+		this.seats = seats;
+	}
+
+
+
+
 	@Override
 	public String toString() {
 		return "Screen [screenNumber=" + screenNumber + ", theatreId=" + theatreId + "]";
