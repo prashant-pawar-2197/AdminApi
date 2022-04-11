c3a3f6b18be66d85b6fb597ea8c2bf0ff0521d31
Rohan Sharma <rohanshrma972@gmail.com>
2022-04-11 12:43:19 +0530

    added scheduler for updating shows after the end time has crossed

--- src/main/java/com/app/controller/LoginController.java
+++ src/main/java/com/app/controller/LoginController.java
@@ -3,7 +3,6 @@ package com.app.controller;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.app.dao.UserRepository;
 import com.app.pojos.User;
 import com.app.service.IUserService;
 
